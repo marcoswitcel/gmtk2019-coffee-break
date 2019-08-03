6147acd159ddbc1b41e2c9cc3394d800a618b9e7
Rodrigo <rodrigozfreddo22@gmail.com>
2019-08-03 18:18:11 -0300

    loader assets

--- js/loadingifmanager.js
+++ js/loadingifmanager.js
@@ -1,12 +1,33 @@
 var LoadManager = (function() {
 	var index = 0;
-
+	var checkAllAssetsLoaded = function(list, callback) {
+		for (var i = list.length - 1; i >= 0; i--) {
+			if (!list[i]) {
+				return;
+			}
+		}
+		callback();
+	}
 	return {
 
-		loadAllAssets: function(jason, callback) {
-			setTimeout(function() {
-				callback();
-			}, 2000);
+		loadAllAssets: function(json, callback) {
+			posicoes = [];
+
+			for (var i = json.assetsResources.length; i--;) {
+				posicoes[i] = false;
+			}
+
+			for (var i = json.assetsResources.length; i--;) {
+				(function() {
+					var a = i;
+					img = new Image();
+					img.onload = function(){
+						posicoes[a] = true;
+						checkAllAssetsLoaded(posicoes, callback);
+					};
+				})()
+				img.src = json.assetsResources[i];
+			}
 		}
 	};
 })()
